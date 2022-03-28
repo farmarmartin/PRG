1ea58a7307bdcc2af3e6797853f5018b422eeed3
Martin Job <90215739+farmarmartin@users.noreply.github.com>
2022-03-29 00:11:25 +0200

    Add files via upload

---
+++ digital_signature/index.php
@@ -0,0 +1,27 @@
+<?php
+include 'dat/dbh.php';
+include 'dat/signature.php';
+?>
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="background-color: darkslategray; text-align: center">
+
+
+<form action="dat/form_data.php" method="POST">
+        <input type="text" name="firstname" placeholder="name">
+        <br>
+        <input type="text" name="surname" placeholder="surname">
+        <br>
+        <input type="date" name="expire" placeholder="date of expiration">
+        <br>
+        <button type="submit" name="submit">submit</button>
+    </form>
+</body>
+</html>
+
