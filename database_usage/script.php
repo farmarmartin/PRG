780d4d21bbbc063f07bf627f359119af5eebc836
Martin Job <90215739+farmarmartin@users.noreply.github.com>
2022-04-25 14:11:25 +0200

    changes in css

--- database_usage/script.php
+++ database_usage/script.php
@@ -11,12 +11,14 @@ $sql = "SELECT code, from_dttm, from_airport_code, to_dttm, to_airport_code, gat
 $result = mysqli_query($connect, $sql);
 
 echo '<table>';
+echo '<tr class="legend">';
 echo '<td><h3>let</h3></td>';
 echo '<td><h3>čas odletu</h3></td>';
 echo '<td><h3>z letiště</h3></td>';
 echo '<td><h3>čas příletu</h3></td>';
 echo '<td><h3>na letiště</h3></td>';
 echo '<td><h3>brána</h3></td>';
+echo '</tr>';
 
     while($row = mysqli_fetch_assoc($result)){
         echo '<tr>';
